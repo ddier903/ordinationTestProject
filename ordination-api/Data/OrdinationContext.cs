bceddfd6ef928ff2cb85cf8f7c006714eb1e3918
ddier903 <victorot@hotmail.dk>
2025-05-06 19:41:47 +0200

    s

--- ordination-api/Data/OrdinationContext.cs
+++ ordination-api/Data/OrdinationContext.cs
@@ -11,10 +11,11 @@ namespace Data
         public DbSet<DagligSkæv> DagligSkæve => Set<DagligSkæv>();
         public DbSet<Laegemiddel> Laegemiddler => Set<Laegemiddel>();
         public DbSet<Ordination> Ordinationer => Set<Ordination>();
-        public DbSet<Dosis> Doser => Set<Dosis>(); // add this if not already there
+        public DbSet<Dosis> Doser => Set<Dosis>(); 
 
         public OrdinationContext(DbContextOptions<OrdinationContext> options) : base(options) { }
 
+        //EF kunne ikke automatisk gemme vores dosis, så vi tilføjer denne kode for at fortælle den hvordan det skal gemmes og hvad det skal linkes sammen med
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<DagligSkæv>()
